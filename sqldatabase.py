79905942633d22872d3d4244ff97b3d7c827866c
wb <wb@Macintosh.local>
2020-09-12 16:22:56 -0700

    cleaned up parsing urls

--- sqldatabase.py
+++ sqldatabase.py
@@ -24,8 +24,8 @@ def insert(data):
 	qdata = data
 	fields = (list(qdata.keys()))[1:-1]
 	values = list(map(str, list(qdata.values())))
-	values[3] = pd.to_datetime(values[3]).date()
-	values[4] = pd.to_datetime(values[4]).date()
+	print(values)
+	print(type(values))
 	sql = "INSERT INTO `voter` (`vuid`, `fname`, `lname`, `regdate`, `bdate`, `maddress`, `raddress`, `precinct`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)" % (values[0], values[1], values[2], values[3], values[4], values[5], values[5], values[7])
 	cursor = db.query(sql)
 	return 0
