503d227794e72cb0ff4bf2c79852a02b9863e9aa
wb <wb@Macintosh.local>
2020-09-12 15:04:46 -0700

    cleaned up parsing urls

--- travis.py
+++ travis.py
@@ -9,9 +9,9 @@ class travis(talker.talker):
 		self.raw = "https://www.votetravis.com/vexpress/submit.do?action=display&searchType=N&electionCode=&electionDateStr=&criteria.firstName=DEREK&maxPollingLocationsDisplay=3&criteria.lastName=HINCH&criteria.vuid=&criteria.month=01&criteria.day=01&criteria.year=1980&earlyVoting=true&address.fullAddress=TX+&address.partialAddress=&address.city=&address.state=TX&address.zipcode="
 		self.bad = b"Voter Information Cannot be Found"
 		self.url = "https://www.votetravis.com/vexpress/submit.do"
-		
+
 	def pdata(self, fname, lname, day, month, year):
-		cache = parse_qs(urlparse(raw).query)
+		cache = parse_qs(urlparse(self.raw).query)
 		cache['criteria.firstName'] = fname
 		cache['lastName'] = lname
 		cache['criteria.month'] = month
