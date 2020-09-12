5a04aefd26540841838736e8aff6128f67ebead1
wb <wb@Macintosh.local>
2020-09-12 11:02:33 -0700

    fixed tabs

--- king.py
+++ king.py
@@ -4,38 +4,38 @@ from bs4 import BeautifulSoup as bs
 
 class king(talker):
 	def __init__(self):
-        self."https://info.kingcounty.gov/kcelections/vote/myvoterinfo.aspx?mode=BBDEDACBFBAAEA/__LASTFOCUS=&__VIEWSTATE=%2FwEPDwUKMTk2ODY3MTEwNWRkjBlwTKM%2BtVtTJVBUu8TnqXjx%2Fgo%3D&__VIEWSTATEGENERATOR=50044A0F&__EVENTTARGET=&__EVENTARGUMENT=&__EVENTVALIDATION=%2FwEdAAYqIbCOYpBRoC5cyVMaV1UJfXpRWHcFD3xcLLL8GEDQecqhKm6XNq2qvCsXIfChktWtnqJgHjbcnqdE2lemm9J5FYn6O78xAbZqtTP8hIE7jpbTm5MhD69V6cvciyfPqhjOWYTm9SoQo8jBWAlCgue3B59%2Fwg%3D%3D&ctl00%24kcMasterPagePlaceHolder%24voterlogin%24firstname=FeiFei&ctl00%24kcMasterPagePlaceHolder%24voterlogin%24lastname=Zhang&ctl00%24kcMasterPagePlaceHolder%24voterlogin%24dateofbirth=03%2F27%2F1978&ctl00%24kcMasterPagePlaceHolder%24voterlogin%24housenumber=&ctl00%24kcMasterPagePlaceHolder%24voterlogin%24mvpbtnlogin=Submit"
-        self.url = "https://info.kingcounty.gov/kcelections/vote/myvoterinfo.aspx?mode=BBDEDACBFBAAEA/"
-        self.bad = b"unable"
-        self.dobstr = "dateofbirth=03%2F27%2F1978"
-        self.date_url_header = "dateofbirth="
-        self.month_separator = "%2F"
-        self.day_separator = "%2F"
-        self.name_string = "firstname=FeiFei&ctl00%24kcMasterPagePlaceHolder%24voterlogin%24lastname=Zhang"
-        self.fname_separator = "firstname="
-        self.lname_separator = "&ctl00%24kcMasterPagePlaceHolder%24voterlogin%24lastname="
+		self."https://info.kingcounty.gov/kcelections/vote/myvoterinfo.aspx?mode=BBDEDACBFBAAEA/__LASTFOCUS=&__VIEWSTATE=%2FwEPDwUKMTk2ODY3MTEwNWRkjBlwTKM%2BtVtTJVBUu8TnqXjx%2Fgo%3D&__VIEWSTATEGENERATOR=50044A0F&__EVENTTARGET=&__EVENTARGUMENT=&__EVENTVALIDATION=%2FwEdAAYqIbCOYpBRoC5cyVMaV1UJfXpRWHcFD3xcLLL8GEDQecqhKm6XNq2qvCsXIfChktWtnqJgHjbcnqdE2lemm9J5FYn6O78xAbZqtTP8hIE7jpbTm5MhD69V6cvciyfPqhjOWYTm9SoQo8jBWAlCgue3B59%2Fwg%3D%3D&ctl00%24kcMasterPagePlaceHolder%24voterlogin%24firstname=FeiFei&ctl00%24kcMasterPagePlaceHolder%24voterlogin%24lastname=Zhang&ctl00%24kcMasterPagePlaceHolder%24voterlogin%24dateofbirth=03%2F27%2F1978&ctl00%24kcMasterPagePlaceHolder%24voterlogin%24housenumber=&ctl00%24kcMasterPagePlaceHolder%24voterlogin%24mvpbtnlogin=Submit"
+		self.url = "https://info.kingcounty.gov/kcelections/vote/myvoterinfo.aspx?mode=BBDEDACBFBAAEA/"
+		self.bad = b"unable"
+		self.dobstr = "dateofbirth=03%2F27%2F1978"
+		self.date_url_header = "dateofbirth="
+		self.month_separator = "%2F"
+		self.day_separator = "%2F"
+		self.name_string = "firstname=FeiFei&ctl00%24kcMasterPagePlaceHolder%24voterlogin%24lastname=Zhang"
+		self.fname_separator = "firstname="
+		self.lname_separator = "&ctl00%24kcMasterPagePlaceHolder%24voterlogin%24lastname="
 
 	def jsonify(html):
-    	soup = bs(html, "lxml")
+		soup = bs(html, "lxml")
 	
-    	rstreet = soup.find("span", {"id": "currentelectiondata21_voterewsreginfo_mvpreginfohousestreet"}).split(">")[1].split("<")[0]
-    	rcsz = soup.find("span", {"id": "currentelectiondata21_voterewsreginfo_mvpreginfocitystatezip"}).split(">")[1].split("<")[0]
-    	raddress = rstreet + rcsz
-    	
-    	mstreet = soup.find("span", {"id": "currentelectiondata21_voterewsreginfo_mvpreginfomailinghousestreet"}).split(">")[1].split("<")[0]
-    	mcst = soup.find("span", {"id": "currentelectiondata21_voterewsreginfo_mvpreginfomailingcitystatezip"}).split(">")[1].split("<")[0]
-    	maddress = mstreet + mcst
-    	not_available = "N/A"
+		rstreet = soup.find("span", {"id": "currentelectiondata21_voterewsreginfo_mvpreginfohousestreet"}).split(">")[1].split("<")[0]
+		rcsz = soup.find("span", {"id": "currentelectiondata21_voterewsreginfo_mvpreginfocitystatezip"}).split(">")[1].split("<")[0]
+		raddress = rstreet + rcsz
+		
+		mstreet = soup.find("span", {"id": "currentelectiondata21_voterewsreginfo_mvpreginfomailinghousestreet"}).split(">")[1].split("<")[0]
+		mcst = soup.find("span", {"id": "currentelectiondata21_voterewsreginfo_mvpreginfomailingcitystatezip"}).split(">")[1].split("<")[0]
+		maddress = mstreet + mcst
+		not_available = "N/A"
 	
-    	values_dict = {
-    	    "vuid": not_available,
-    	    "fname": soup.find("span", {"class": "mvi-my-name-first"}).split(">")[1].split("<")[0],
-    	    "lname": soup.find("span", {"class": "mvi-my-name-last"}).split(">")[1].split("<")[0],
-    	    "rdate": not_available,
-    	    "bdate": "",
-    	    "raddress": raddress,
-    	    "maddress": maddress,
-    	    "precinct": precinct = soup.find("span", {"id": "voterreginfo_mvpreginfoprecinct"}).split(">")[1].split("<")[0]
-    	} 
+		values_dict = {
+			"vuid": not_available,
+			"fname": soup.find("span", {"class": "mvi-my-name-first"}).split(">")[1].split("<")[0],
+			"lname": soup.find("span", {"class": "mvi-my-name-last"}).split(">")[1].split("<")[0],
+			"rdate": not_available,
+			"bdate": "",
+			"raddress": raddress,
+			"maddress": maddress,
+			"precinct": precinct = soup.find("span", {"id": "voterreginfo_mvpreginfoprecinct"}).split(">")[1].split("<")[0]
+		} 
 	
-    	return json.dumps(values_dict, indent=0, sort_keys=False, default=None)	
+		return json.dumps(values_dict, indent=0, sort_keys=False, default=None)	
--- talker.py
+++ talker.py
@@ -5,53 +5,53 @@ from urllib.parse import urlparse, parse_qs
 import sqldatabase
 
 class talker():
-    #Base Class for counties
-    def __init__(self):
-    
-    class voter_data:
-        def __init__(self):
-            self.data = ""
-            self.bdate = ""
+	#Base Class for counties
+	def __init__(self):
+	
+	class voter_data:
+		def __init__(self):
+			self.data = ""
+			self.bdate = ""
 
-    #Function looks up voters. It looks in the MySQL database first, then makes a request to the county website
-    #It loops until it finds a voter, and returns the information. If the information is class talker it returns
-    #a talker object, if it's in the database it returns a record from the database.
-    def request(fname, lname, sdate, edate, county):
-        r_data = place[county]
-        raw.replace(r_data.name_string, r_data.fname_separator + fname + r_data.lname_separator + lname)         
-        daterange = pd.date_range(sdate, edate)
-        for i in daterange:
-            if i.month < 10:
-                month = "0" + str(i.month)
-            else:
-                month = str(i.month)
-            if i.day < 10:
-                day = "0" + str(i.day)
-            else:
-                day = str(i.day)
-            bdate = pd.to_datetime(i).date()
-            record = sqldatabase.check_name(bdate, fname, lname) 
-            if record == "notexist":
-                rep_str = r_data.date_url_header + month + r_data.month_separator + day + r_data.day_separator + str(i.year)
-                new_req = req.replace(r_data.dobstr, rep_str)
-                data = parse_qs(urlparse(new_req).query)
-                r = requests.post(r_data.url, data = data)
-                if bad in r.content:
-                    continue
-                else:
-                    v = voter_data
-                    v.bdate = pd.to_datetime(str(i.month) + "/" + str(i.day) + "/" + str(i.year)).date()
-                    v.data = r.content
-                    jdata = jsonify(v)
-                    sqldatabase.insert(jdata)
-                    return jdata
-            else:
-                return record 
+	#Function looks up voters. It looks in the MySQL database first, then makes a request to the county website
+	#It loops until it finds a voter, and returns the information. If the information is class talker it returns
+	#a talker object, if it's in the database it returns a record from the database.
+	def request(fname, lname, sdate, edate, county):
+		r_data = place[county]
+		raw.replace(r_data.name_string, r_data.fname_separator + fname + r_data.lname_separator + lname)         
+		daterange = pd.date_range(sdate, edate)
+		for i in daterange:
+			if i.month < 10:
+				month = "0" + str(i.month)
+			else:
+				month = str(i.month)
+			if i.day < 10:
+				day = "0" + str(i.day)
+			else:
+				day = str(i.day)
+			bdate = pd.to_datetime(i).date()
+			record = sqldatabase.check_name(bdate, fname, lname) 
+			if record == "notexist":
+				rep_str = r_data.date_url_header + month + r_data.month_separator + day + r_data.day_separator + str(i.year)
+				new_req = req.replace(r_data.dobstr, rep_str)
+				data = parse_qs(urlparse(new_req).query)
+				r = requests.post(r_data.url, data = data)
+				if bad in r.content:
+					continue
+				else:
+					v = voter_data
+					v.bdate = pd.to_datetime(str(i.month) + "/" + str(i.day) + "/" + str(i.year)).date()
+					v.data = r.content
+					jdata = jsonify(v)
+					sqldatabase.insert(jdata)
+					return jdata
+			else:
+				return record 
 
-    #Abstract functions becasue the data from each county is different. These will remove HTML and convert to json.
-    #We usually just call soupit from jsonify because we just want json in the view.
-    def jsonify():
-        pass
+	#Abstract functions becasue the data from each county is different. These will remove HTML and convert to json.
+	#We usually just call soupit from jsonify because we just want json in the view.
+	def jsonify():
+		pass
 
-    def soupit():
-        pass
+	def soupit():
+		pass
