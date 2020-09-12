d3a704fe77cfe3512d5824f16584a9aa7c6026d2
wb <wb@Macintosh.local>
2020-09-12 13:46:02 -0700

    tests

--- voteo.py
+++ voteo.py
@@ -27,6 +27,7 @@ def lookup():
     fmat = request.args.get("fmat") if request.args.get("fmat") else "html"
     county = request.args.get("county")    
     t = locale[county]
+    print(locale)
     raw = t.request(fname, lname, sdate, edate)
 
     if raw is None:
