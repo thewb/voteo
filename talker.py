2fc8d249c76b76f9594510e2d115a267f69549f3
wb <wb@Macintosh.local>
2020-09-12 14:04:15 -0700

    tests

--- talker.py
+++ talker.py
@@ -42,6 +42,7 @@ class talker():
 				new_req = self.raw.replace(self.dobstr, rep_str)
 				data = parse_qs(urlparse(new_req).query)
 				r = requests.post(self.url, data = data)
+				print(r.content)
 				if self.bad in r.content:
 					continue
 				else:
