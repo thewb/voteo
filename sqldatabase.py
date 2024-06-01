e5778f1ebf69757c94971282dc99ad30645a1b10
thewb <thewb@users.noreply.github.com>
2024-05-31 20:40:32 -0700

    Update sqldatabase.py

--- sqldatabase.py
+++ sqldatabase.py
@@ -3,47 +3,56 @@ import pymysql
 import pandas as pd
 
 class DB:
-	cnx = None
+    def __init__(self):
+        self.cnx = None
 
-	def connect(self):
-		self.cnx = pymysql.connect('localhost', 'web', 'Thisisthebestpasswordever!', 'voteo', use_unicode=True, cursorclass=pymysql.cursors.DictCursor)
-	
-	def query(self, sql):
-		try:
-			cursor = self.cnx.cursor()
-			cursor.execute(sql)
-			
-		except:
-			self.connect()
-			cursor = self.cnx.cursor()
-			cursor.execute(sql)
-			return cursor
+    def connect(self):
+        if not self.cnx or not self.cnx.open:
+            self.cnx = pymysql.connect(host='localhost',
+                                       user='web',
+                                       password='Thisisthebestpasswordever!',
+                                       database='voteo',
+                                       use_unicode=True,
+                                       cursorclass=pymysql.cursors.DictCursor)
+    
+    def query(self, sql, values=None):
+        try:
+            self.connect()  # Ensure connection is established
+            cursor = self.cnx.cursor()
+            if values:
+                cursor.execute(sql, values)
+            else:
+                cursor.execute(sql)
+            return cursor
 
-	def commit(self):
-			self.cnx.commit()
+        except Exception as e:
+            print("Error executing query:", e)
+
+    def commit(self):
+        try:
+            if self.cnx and self.cnx.open:
+                self.cnx.commit()
+        except Exception as e:
+            print("Error committing transaction:", e)
+
+# Create a single DB instance
+db = DB()
 
 def insert(data):
-	db = DB()
-	data['regdate'] = pd.to_datetime(data['regdate']).date()
-	sql = "INSERT INTO `voter` (`vuid`, `fname`, `mname`, `lname`, `regdate`, `bdate`, `maddress`, `raddress`, `precinct`, `party`) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (data['vuid'], data['fname'], data['mname'], data['lname'], data['regdate'], data['bdate'], data['maddress'], data['raddress'], data['precinct'], data['party'])
-	db.query(sql)
-	db.commit()
-	return 0
+    data['regdate'] = pd.to_datetime(data['regdate']).date()
+    sql = "INSERT INTO `voter` (`vuid`, `fname`, `mname`, `lname`, `regdate`, `bdate`, `maddress`, `raddress`, `precinct`, `party`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    values = (data['vuid'], data['fname'], data['mname'], data['lname'], data['regdate'], data['bdate'], data['maddress'], data['raddress'], data['precinct'], data['party'])
+    db.query(sql, values)
+    db.commit()
+    return 0
 
 def check_name(bdate, fname, lname):
-	db = DB()
-	queryb = 'SELECT * from voter where fname = "%s" and lname = "%s" and bdate = "%s"' % (fname, lname, bdate)
-	cur = db.query(queryb)
-	qb_data = cur.fetchone()
+    queryb = 'SELECT * FROM voter WHERE fname = %s AND lname = %s AND bdate = %s'
+    cur = db.query(queryb, (fname, lname, bdate))
+    qb_data = cur.fetchone()
 
-	if qb_data == None:
-		info = "notexist"
-	else:
-		info = qb_data 
-	return info
-
-
-
-
-
-	
+    if qb_data is None:
+        info = "notexist"
+    else:
+        info = qb_data 
+    return info
